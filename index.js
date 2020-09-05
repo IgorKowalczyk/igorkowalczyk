24375284e2fa1b42e3b5dccca82476348d3c4e76
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-09-05 11:11:28 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+import fs from "fs";
+import fetch from "node-fetch";
+import parser from "xml2json";
+
+try {
+ const FEED_URL = "https://igorkowalczyk.github.io/blog/feeds/feed.xml";
+ const TAG_OPEN = `<!-- FEED-START -->`;
+ const TAG_CLOSE = `<!-- FEED-END -->`;
+
+ const fetchArticles = async () => {
+  const articles = await fetch(FEED_URL);
+  const articlesText = await articles.text();
+  const articlesJSON = parser.toJson(articlesText);
+  const newC = JSON.parse(articlesJSON).rss.channel.item.slice(0, 5);
+  return newC.map(({ title, link }) => `- [${title}](${link})`).join("\n");
+ };
+
+  const readme = fs.readFileSync("./README.md", "utf8");
+  const indexBefore = readme.indexOf(TAG_OPEN) + TAG_OPEN.length;
+  const indexAfter = readme.indexOf(TAG_CLOSE);
+  const readmeContentChunkBreakBefore = readme.substring(0, indexBefore);
+  const readmeContentChunkBreakAfter = readme.substring(indexAfter);
+
+  const posts = await fetchArticles();
+
+  const readmeNew = `
+   ${readmeContentChunkBreakBefore}
+   ${posts}
+   ${readmeContentChunkBreakAfter}
+  `;
+
+  fs.writeFileSync("./README.md", readmeNew.trim());
+} catch (err) {
+ console.error(err);
+}
