1f1a73295a8ae2015654e01f608204a894e2ff65
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-05-31 14:44:39 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ const fetch = require("node-fetch");
 const parser = require("xml2json");
 const moment = require("moment");
 const date = new Date();
-const open = `<!-- FEED-START -->`;
-const close = `<!-- FEED-END -->`;
-const open2 = `<!-- STAT-START -->`;
-const close2 = `<!-- STAT-END -->`;
+const feed_open = `<!-- FEED-START -->`;
+const feed_close = `<!-- FEED-END -->`;
+const stats_open = `<!-- STAT-START -->`;
+const stats_close = `<!-- STAT-END -->`;
 
 try {
  const fetchArticles = async () => {
@@ -16,14 +16,15 @@ try {
   const newC = JSON.parse(articlesJSON).rss.channel.item.slice(0, 5);
   return newC.map(({ title, link, pubDate }) => `- [${title}](${link}) [${moment(pubDate).format("D MMM YYYY")}]`).join("\n") + `\n<!-- Posts last updated on ${date.toString()} -->`;
  };
-
  const fetchGithub = async () => {
   const user = await fetch('https://api.github.com/users/igorkowalczyk').then(res => res.json())
-  const stars = await fetch(`https://api.github.com/users/igorkowalczyk/starred`).then(res => res.json())
-  stars.forEach(star => {
-   const allstars = allstars++;
-  })
   return `
+  - 📚 Total Repositories: **${user.public_repos}**
+  - 📖 Total Gists: **${user.public_gists}**
+  <!-- Stats last updated on ${date.toString()} -->`
+ };
+ /*
+   return `
   - ⭐ Total Stars: **${allstars}**
   - 🕚 Total Commits: **9483**
   - 📚 Total Repositories: **${user.public_repos}**
@@ -33,29 +34,30 @@ try {
   - 📝 Contributed to: **4**
   <!-- Posts last updated on ${date.toString()} -->`
  };
+ */
  (async () => {
   const readme = fs.readFileSync("./README.md", "utf8");
-  const indexBefore = readme.indexOf(open) + open.length;
-  const indexAfter = readme.indexOf(close);
-  const readmeContentChunkBreakBefore = readme.substring(0, indexBefore);
-  const readmeContentChunkBreakAfter = readme.substring(indexAfter);
+  const feed_before = readme.indexOf(feed_open) + feed_open.length;
+  const feed_after = readme.indexOf(feed_close);
+  const feed_before_final = readme.substring(0, feed_before);
+  const feed_after_final = readme.substring(feed_after);
   const posts = await fetchArticles();
-  const readmeNew = `
-   ${readmeContentChunkBreakBefore}
+  const readme_feed = `
+   ${feed_before_final}
    ${posts}
-   ${readmeContentChunkBreakAfter}
+   ${feed_after_final}
   `;
-  const indexBefore2 = readmeNew.indexOf(open2) + open.length;
-  const indexAfter2 = readmeNew.indexOf(close2);
-  const readmeContentChunkBreakBefore2 = readmeNew.substring(0, indexBefore2);
-  const readmeContentChunkBreakAfter2 = readmeNew.substring(indexAfter2);
+  const stats_before = readme_feed.indexOf(stats_open) + stats_open.length;
+  const stats_after = readme_feed.indexOf(stats_close);
+  const stats_before_final = readme_feed.substring(0, stats_before);
+  const stats_after_final = readme_feed.substring(stats_after);
   const stats = await fetchGithub();
-  const readmeFinal = `
-  ${readmeContentChunkBreakBefore2}
+  const readme_final = `
+  ${stats_before_final}
   ${stats}
-  ${readmeContentChunkBreakAfter2}
+  ${stats_after_final}
  `;
- fs.writeFileSync("./README.md", readmeFinal.trim());
+ fs.writeFileSync("./README.md", readme_final.trim());
  })();
 } catch (err) {
  return console.error(err);
