4a32f3983ea13a87d318a444218755aa393126cb
Igor Kowalczyk <xxmajonezxx@gmail.com>
2022-09-07 14:15:38 +0200

    Update fetch_posts.js

--- src/updaters/fetch_posts.js
+++ src/updaters/fetch_posts.js
@@ -9,7 +9,7 @@ export async function fetch_posts(xml) {
  let content;
  console.log(`::debug:: Fetching ${xml}`);
  await fetch(xml).then(async (res) => {
-  if (!res.headers.get("content-type").includes("xml")) throw new Error("Link must be an xml file!");
+  //if (!res.headers.get("content-type").includes("xml")) throw new Error("Link must be an xml file!");
   const rss = JSON.parse(toJson(await res.text())).rss.channel.item.slice(0, feed.max_lines || 5);
   console.log(`::debug:: Fetched ${xml}, ${rss.length} posts found`);
   content = rss.map(({ title, link, date }) => `- [${title}](${link}) \`[${moment(date).format("DD/MM/YYYY")}]\``).join("\n") + `\n<!-- Posts last updated on ${new Date().toString()} -->`;
