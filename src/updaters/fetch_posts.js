918b270686a836552dfd2e9ad736e031e2de82b7
IgorKowalczyk <IgorKowalczyk@users.noreply.github.com>
2022-09-07 13:01:18 +0000

    [🚀] Code formatting

--- src/updaters/fetch_posts.js
+++ src/updaters/fetch_posts.js
@@ -10,10 +10,10 @@ export async function fetch_posts(xml) {
  console.log(`::debug:: Fetching ${xml}`);
  await fetch(xml).then(async (res) => {
   if (!res.headers.get("content-type").includes("xml")) throw new Error("Link must be an xml file!");
-  const xml_body = await res.text()
+  const xml_body = await res.text();
   parseString(xml_body, function (err, result) {
-   content = result.rss.channel[0].item.map(({title, link, pubDate}) => `- [${title}](${link}) \`[${moment(pubDate).format("DD/MM/YYYY")}]\``).join("\n") + `\n<!-- Posts last updated on ${new Date().toString()} -->`
-  })
- })
+   content = result.rss.channel[0].item.map(({ title, link, pubDate }) => `- [${title}](${link}) \`[${moment(pubDate).format("DD/MM/YYYY")}]\``).join("\n") + `\n<!-- Posts last updated on ${new Date().toString()} -->`;
+  });
+ });
  return content;
 }
