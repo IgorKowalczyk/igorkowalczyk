f6097b07f39942a9dad641431871f2dc1dcdaa5d
Igor Kowalczyk <majonezexe@protonmail.com>
2022-09-08 18:57:35 +0200

    Fix post updater

--- src/updaters/fetch_posts.js
+++ src/updaters/fetch_posts.js
@@ -1,7 +1,7 @@
 import { feed } from "../config.js";
 import { parseString } from "xml2js";
 import fetch from "node-fetch";
-import moment from "moment";
+import { format } from "date-fns";
 
 export async function fetch_posts(xml) {
  if (!xml) throw new Error("You must provide a link to get feed!");
@@ -12,7 +12,11 @@ export async function fetch_posts(xml) {
   if (!res.headers.get("content-type").includes("xml")) throw new Error("Link must be an xml file!");
   const xml_body = await res.text();
   parseString(xml_body, function (err, result) {
-   content = result.rss.channel[0].item.map(({ title, link, pubDate }) => `- [${title}](${link}) \`[${moment(pubDate).format("DD/MM/YYYY")}]\``).join("\n") + `\n<!-- Posts last updated on ${new Date().toString()} -->`;
+   content =
+    result.rss.channel[0].item
+     .slice(0, feed.max_lines || 5)
+     .map(({ title, link, pubDate }) => `- [${title}](${link}) \`[${format(new Date(pubDate[0]), "MMMM dd, yyyy")}]\``)
+     .join("\n") + `\n<!-- Posts last updated on ${new Date().toString()} -->}`;
   });
  });
  return content;
