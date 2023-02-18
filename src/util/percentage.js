36bb498378e6e9f994f0383de1c490e133c63831
Igor Kowalczyk <majonezexe@protonmail.com>
2023-02-18 10:22:00 +0100

    Optimize percentage bar

--- src/util/percentage.js
+++ src/util/percentage.js
@@ -1,15 +1,13 @@
-export function percentageBar(full, curr) {
+export function percentageBar(full, curr, numBars = 25) {
  if (full <= 0) return "Error: Full value must be greater than 0.";
  if (curr < 0) return "Error: Current value must be greater than or equal to 0.";
  if (curr > full) return "Error: Current value must be less than or equal to full value.";
+ 
  const percent = (curr / full) * 100;
- let str = "[";
- for (let i = 0; i < 25; i++) {
-  str += percent >= (100 / 25) * (i + 1) ? "▓" : "░";
- }
- str += `]   ${percent.toFixed(2)}%`;
-
- return str;
+ const numCompleteBars = Math.floor(percent / (100 / numBars));
+ const numEmptyBars = numBars - numCompleteBars;
+ const completeBar = "█".repeat(numCompleteBars);
+ const emptyBar = "░".repeat(numEmptyBars);
+ 
+ return `[${completeBar}${emptyBar}]   ${percent.toFixed(2)}%`;
 }
-
-// █
