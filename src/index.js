9f9337819cddf26ab90742ef1d52a7ec728cf452
Igor Kowalczyk <majonezexe@protonmail.com>
2023-08-30 17:10:31 +0200

    Remove old breaklines

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const readme = await Deno.readTextFile("./README.md", "utf8");
 const [posts, activityList, statsList] = await Promise.all([fetchPosts(feed.link), fetchActivities(activity.gitUsername), fetchCodingStats(wakatime.apiKey, activity.gitUsername)]);
 
 const readmePosts = `${readme.substring(0, readme.indexOf(feed.open) + feed.open.length)}\n${posts}\n${readme.substring(readme.indexOf(feed.close))}`;
-const readmeActivity = `${readmePosts.substring(0, readmePosts.indexOf(activity.open) + activity.open.length)}\n${activityList.join("<br>")}\n${readmePosts.substring(readmePosts.indexOf(activity.close))}`;
+const readmeActivity = `${readmePosts.substring(0, readmePosts.indexOf(activity.open) + activity.open.length)}\n${activityList.join("<br/>")}\n${readmePosts.substring(readmePosts.indexOf(activity.close))}`;
 const wakatimeActivity = `${readmeActivity.substring(0, readmeActivity.indexOf(wakatime.open) + wakatime.open.length)}\n${statsList}\n${readmeActivity.substring(readmeActivity.indexOf(wakatime.close))}`;
 await Deno.writeTextFile("./README.md", wakatimeActivity.trim());
 
