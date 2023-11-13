2b0305818a8ab798b2db728202b7dbd88c3ea8db
Igor Kowalczyk <majonezexe@protonmail.com>
2023-11-13 20:02:35 +0100

    Fix posts

--- src/updaters/fetchPosts.js
+++ src/updaters/fetchPosts.js
@@ -5,17 +5,19 @@ import { feed } from "../config.js";
 export async function fetchPosts(xml) {
  if (!xml) throw new Error("You must provide a link to get feed!");
  console.log(`::debug:: [Posts] Fetching ${xml}`);
- 
+
  const res = await fetch(xml);
  if (!res.headers.get("content-type").includes("xml")) throw new Error("Link must be an xml file!");
  const xmlBody = await res.text();
 
- parseString(xmlBody, (err, result) => {
+ await parseString(xmlBody, (err, result) => {
   if (err) throw new Error(err);
   console.log(`::debug:: [Posts] Fetched ${result.feed.entry.length} posts`);
-  return result.feed.entry
+  const content =  result.feed.entry
    .slice(0, feed.maxLines || 5)
    .map(({ title, id, updated }) => `- [${title}](${id}) \`[${format(new Date(updated[0]), "MMMM dd, yyyy")}]\``)
    .join("\n") + `\n<!-- Posts last updated on ${new Date().toString()} -->`;
+
+  return content;
  });
 }
